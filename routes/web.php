e670f11f2a4af4f4e1c3233733e9aaf6782a32e2
Justin Oliver <52309338+joehOliver@users.noreply.github.com>
2021-03-11 10:29:04 +0800

    added images

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,15 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+/*
+
+Route::get('/deck/bridge', function () {
+    return view('bridge');
+});
+
+Route::get('/about')
+
+Route::get('/contact')
+
+*/
