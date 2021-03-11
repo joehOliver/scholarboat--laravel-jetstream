3a573ebe7ffb7b94e847f9017203a497d605e9af
Justin Oliver <52309338+joehOliver@users.noreply.github.com>
2021-03-11 18:19:02 +0800

    mail configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
 
 module.exports = {
     purge: [
@@ -11,9 +12,32 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Atkinson Hyperlegible', ...defaultTheme.fontFamily.sans],
             },
         },
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+      
+            black: {
+              light: '#292929',
+              DEFAULT: '#121212',
+              dark: '#000000'
+            },
+            white: colors.white,
+            gray: colors.gray,
+            red: colors.red,
+            yellow: {
+              light: '#fdff87',
+              DEFAULT: '#FAFF03',
+              dark: '#ffe100'
+            },
+            green: colors.emerald,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            purple: colors.violet,
+            pink: colors.pink,
+          },
     },
 
     variants: {
